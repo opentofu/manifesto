64675417689815124929ca0f080f06078b527058
Flavius <flaviuscdinu93@gmail.com>
2023-08-18 10:34:34 +0300

    Added counter for Companies, Projects and Individuals

---
+++ count.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+    let counts = {
+        'Company': 0,
+        'Project': 0,
+        'Individual': 0
+    };
+
+    let rows = document.querySelectorAll('table tr');
+
+    rows.forEach(row => {
+        let cells = row.querySelectorAll('td');
+        cells.forEach(cell => {
+            if (counts.hasOwnProperty(cell.textContent)) {
+                counts[cell.textContent]++;
+            }
+        });
+    });
+    
+    document.querySelector('#companyOutput .count').textContent = counts['Company'];
+    document.querySelector('#projectOutput .count').textContent = counts['Project'];
+    document.querySelector('#individualOutput .count').textContent = counts['Individual'];
+};
